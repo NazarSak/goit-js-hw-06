e1aca881142a206528bd99055762895d8936292c
NazarSak <112251832+NazarSak@users.noreply.github.com>
2022-12-24 21:40:01 +0200

    10

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const button = document.querySelector(".change-color")
+console.log(button)
+
+const span = document.querySelector(".color")
+console.log(span);
+
+button.addEventListener("click",chnageColorFunction)
+
+function chnageColorFunction () {
+  const colors = getRandomHexColor();
+  document.body.style.backgroundColor = colors;
+  span.textContent = colors 
+ 
+
+}
